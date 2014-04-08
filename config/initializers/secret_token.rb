2b72d4c15343659144f877725b633e7baaae0d78
cd Users\C502user-01\RubymineProjects\projektni <cd users>
2014-04-08 11:45:23 +0200

    prvic delam git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projektni::Application.config.secret_token = 'a65baece4bf4e4230ffe2e6ea082c7e77ff5e7e07f0ef7208d219591c7f3904738c116114bd9639e315b098eb9dd16786332f3b0665ce8930546fa90aad848a9'
